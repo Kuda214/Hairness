bd21f6160f7f6bc42334eae34d2cecbbee4cb49a
kuda214 <kudachivunga33@gmail.com>
2022-07-06 01:28:53 +0200

    🎉 Expo Init to begin the proect

---
+++ Hairness/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import HairItem from './assets/components/hairItem/';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <HairItem/>
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  
+
+});
